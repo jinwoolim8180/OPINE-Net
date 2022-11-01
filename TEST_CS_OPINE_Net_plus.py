d0a6dae4818598fae3b738849c265b0e579fc24a
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-11-01 12:14:22 +0900

    init

--- TEST_CS_OPINE_Net_plus.py
+++ TEST_CS_OPINE_Net_plus.py
@@ -14,6 +14,8 @@ import cv2
 from skimage.measure import compare_ssim as ssim
 from argparse import ArgumentParser
 from utility_for_opinenet import *
+from models import OPINENetplus
+import models
 
 parser = ArgumentParser(description='OPINE-Net-plus')
 
@@ -38,6 +40,7 @@ learning_rate = args.learning_rate
 layer_num = args.layer_num
 group_num = args.group_num
 cs_ratio = args.cs_ratio
+models.cs_ratio = args.cs_ratio
 gpu_list = args.gpu_list
 test_name = args.test_name
 
@@ -54,120 +57,6 @@ n_input = ratio_dict[cs_ratio]
 n_output = 1089
 nrtrain = 88912
 batch_size = 64
-
-
-class MySign(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, input):
-        output = input.new(input.size())
-        output[input >= 0] = 1
-        output[input < 0] = -1
-        return output
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        grad_input = grad_output.clone()
-        return grad_input
-
-MyBinarize = MySign.apply
-
-
-
-# Define OPINE-Net Block
-class BasicBlock(torch.nn.Module):
-    def __init__(self):
-        super(BasicBlock, self).__init__()
-
-        self.lambda_step = nn.Parameter(torch.Tensor([0.5]))
-        self.soft_thr = nn.Parameter(torch.Tensor([0.01]))
-
-        self.conv_D = nn.Parameter(init.xavier_normal_(torch.Tensor(32, 1, 3, 3)))
-
-        self.conv1_forward = nn.Parameter(init.xavier_normal_(torch.Tensor(32, 32, 3, 3)))
-        self.conv2_forward = nn.Parameter(init.xavier_normal_(torch.Tensor(32, 32, 3, 3)))
-        self.conv1_backward = nn.Parameter(init.xavier_normal_(torch.Tensor(32, 32, 3, 3)))
-        self.conv2_backward = nn.Parameter(init.xavier_normal_(torch.Tensor(32, 32, 3, 3)))
-
-        self.conv1_G = nn.Parameter(init.xavier_normal_(torch.Tensor(32, 32, 3, 3)))
-        self.conv2_G = nn.Parameter(init.xavier_normal_(torch.Tensor(32, 32, 3, 3)))
-        self.conv3_G = nn.Parameter(init.xavier_normal_(torch.Tensor(1, 32, 3, 3)))
-
-    def forward(self, x, PhiWeight, PhiTWeight, PhiTb):
-        x = x - self.lambda_step * PhiTPhi_fun(x, PhiWeight, PhiTWeight)
-        x = x + self.lambda_step * PhiTb
-        x_input = x
-
-        x_D = F.conv2d(x_input, self.conv_D, padding=1)
-
-        x = F.conv2d(x_D, self.conv1_forward, padding=1)
-        x = F.relu(x)
-        x_forward = F.conv2d(x, self.conv2_forward, padding=1)
-
-        x = torch.mul(torch.sign(x_forward), F.relu(torch.abs(x_forward) - self.soft_thr))
-
-        x = F.conv2d(x, self.conv1_backward, padding=1)
-        x = F.relu(x)
-        x_backward = F.conv2d(x, self.conv2_backward, padding=1)
-
-        x = F.conv2d(F.relu(x_backward), self.conv1_G, padding=1)
-        x = F.conv2d(F.relu(x), self.conv2_G, padding=1)
-        x_G = F.conv2d(x, self.conv3_G, padding=1)
-
-        x_pred = x_input + x_G
-
-        x = F.conv2d(x_forward, self.conv1_backward, padding=1)
-        x = F.relu(x)
-        x_D_est = F.conv2d(x, self.conv2_backward, padding=1)
-        symloss = x_D_est - x_D
-
-        return [x_pred, symloss]
-
-
-# Define OPINE-Net-plus
-class OPINENetplus(torch.nn.Module):
-    def __init__(self, LayerNo, n_input):
-        super(OPINENetplus, self).__init__()
-
-        self.Phi = nn.Parameter(init.xavier_normal_(torch.Tensor(n_input, 1089)))
-        self.Phi_scale = nn.Parameter(torch.Tensor([0.01]))
-
-        onelayer = []
-        self.LayerNo = LayerNo
-
-        for i in range(LayerNo):
-            onelayer.append(BasicBlock())
-
-        self.fcs = nn.ModuleList(onelayer)
-
-    def forward(self, x):
-
-        # Sampling-subnet
-        Phi_ = MyBinarize(self.Phi)
-        Phi = self.Phi_scale * Phi_
-        PhiWeight = Phi.contiguous().view(n_input, 1, 33, 33)
-        Phix = F.conv2d(x, PhiWeight, padding=0, stride=33, bias=None)    # Get measurements
-
-        # Initialization-subnet
-        PhiTWeight = Phi.t().contiguous().view(n_output, n_input, 1, 1)
-        PhiTb = F.conv2d(Phix, PhiTWeight, padding=0, bias=None)
-        PhiTb = torch.nn.PixelShuffle(33)(PhiTb)
-        x = PhiTb    # Conduct initialization
-
-        # Recovery-subnet
-        layers_sym = []   # for computing symmetric loss
-        for i in range(self.LayerNo):
-            [x, layer_sym] = self.fcs[i](x, PhiWeight, PhiTWeight, PhiTb)
-            layers_sym.append(layer_sym)
-
-        x_final = x
-
-        return [x_final, layers_sym, Phi]
-
-
-def PhiTPhi_fun(x, PhiW, PhiTW):
-    temp = F.conv2d(x, PhiW, padding=0,stride=33, bias=None)
-    temp = F.conv2d(temp, PhiTW, padding=0, bias=None)
-    return torch.nn.PixelShuffle(33)(temp)
 
 
 model = OPINENetplus(layer_num, n_input)
